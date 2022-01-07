f00952ed21d0d9ea4c25845c5ec36bf92ab82008
Yichi Zhang <77417378+yichi-rex@users.noreply.github.com>
2022-01-08 06:40:49 +0800

    Update PriceProblem.java

--- Test_Part2/PriceProblem.java
+++ Test_Part2/PriceProblem.java
@@ -24,12 +24,34 @@ public class PriceProblem{
                 type = 0;
                 break;
         }
-        result = e.getPrice() * 1.05 * (type+ 0.012*Double.valueOf(e.getPeople));
+        result = e.getPrice() * 1.05 * (type+ 0.012*Double.valueOf(e.getPeople()));
         return result;
     }
+
+    public String readCategory(){
+        System.out.println();
+    }
+
     public void run(){
-        Productions e = new Productions("food",1299.99,3);
-        System.out.println(calculate(e));
+        Scanner sc = new Scanner(System.in);
+        boolean flag = true;
+        while (flag == true){
+            System.out.println("1= Get Input; Other number = End");
+            try{
+                int choose = sc.nextInt();
+                if(choose == 1){
+                    Productions e = new Productions("food",1299.99,3);
+                    System.out.println(calculate(e));
+                }else{
+                    flag = false;
+                    break;
+                }
+            }catch(Exception e){
+                System.out.println("Need to enter integer number!");
+                run();
+            }
+        }
+        sc.close(); 
     }
     public static void main(String args[]){
         new PriceProblem().run();
